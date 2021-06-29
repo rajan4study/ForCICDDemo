4447df68a1c83e86d50cb88ac99a44a796bbcace
rajan4Study <rajan943@gmail.com>
2021-06-29 13:31:53 +0530

    for cicd

---
+++ src/main/java/com/study/cicd/controller/ScopeController.java
@@ -0,0 +1,33 @@
+package com.study.cicd.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.study.cicd.service.SingletonPojo;
+
+/**
+ * @author rajan
+ * 
+ * ScopeContoller- rest controller class
+ *
+ */
+
+@RestController
+public class ScopeController {
+	
+	@Autowired
+	private SingletonPojo singletonPojo;
+	
+	
+	@GetMapping("/getDate")
+	public List<String> getMethod() throws InterruptedException{
+		String first=singletonPojo.displayDate();
+		Thread.sleep(2000);
+		String second=singletonPojo.displayDate();
+		return Arrays.asList(first,second);
+	}
+
+}
